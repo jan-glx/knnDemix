6e30b73a0847ca4204165b9377f2f93f441c8e27
Jan Gleixner <jan.gleixner@gmail.com>
2016-12-07 21:43:15 +0100

    numerical improvement changed optimization to act in domain of the test statistic instead of p.values for numerical reasons

--- R/knn_mixture_test.R
+++ R/knn_mixture_test.R
@@ -54,19 +54,20 @@ mixture.test <- function(X0, Xm, alpha = 1, alternative = "lower", conf.level = 
   #pchisq(S, lower.tail = FALSE, df = 2*nm) <= (1-conf.level)
   #S <= qchisq((1-conf.level), lower.tail = FALSE, df = 2*nm)
   #-2*sum(log(p.vals))
+  S_upper <- qchisq(conf.level, df=2*nm)
   cint <-  if (!calc.CI) c(0,1) else c(0,  pmax(optimize(function(alpha) {
     p.vals <- extraDistr::pbetapr(alpha/r,k,k,lower.tail = FALSE)
     S <- -2*sum(log(p.vals))
     pval <- pchisq(S, lower.tail = FALSE, df = 2*nm)
-    (pval-(1-conf.level))^2
+    abs(S-S_upper)
   },c(0,1))$minimum,0) ) #there should be a smarter way...
   attr(cint, "conf.level") <- conf.level
 
+  S_med <- qchisq(0.5, df=2*nm)
   estimate <- if (!calc.CI) 0 else min(1, pmax(optimize(function(alpha) {
     p.vals <- extraDistr::pbetapr(alpha/r,k,k,lower.tail = FALSE)
     S <- -2*sum(log(p.vals))
-    pval <- pchisq(S, lower.tail = FALSE, df = 2*nm)
-    (pval-0.5)^2
+    abs(S-S_med)
   },c(0,1))$minimum,0) )
   names(estimate) <- "alpha_0"
 
