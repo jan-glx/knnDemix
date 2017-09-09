7e3731b8a408f95231d89316314b1330dbfa12da
Jan Gleixner <jan.gleixner@gmail.com>
2017-09-10 01:11:57 +0200

    fix handling of vector inputs
    and added test for vector inputs

--- tests/testthat/test_mixture.test.R
+++ tests/testthat/test_mixture.test.R
@@ -42,4 +42,23 @@ test_that("mixture.test for scewed sample numbers", {
     1) #should exclude 1
 })
 
+
+n0 <- 5000 # 100 sample points from \eqn{F_0}
+nm <- 500 # 100 sample points from \eqn{F_0}
+n1 <- 250  # of which 50 are from \eqn{F_1}
+X0 <- rnorm(n0) # \eqn{F_0} is standard normal, \eqn{F_1} has mean 10:
+Xm <- c(rnorm((nm-n1)), rnorm(n1)+10)
+
+test_that("mixture.test with vectors", {
+  expect_gt(
+    mixture.test(X0, Xm, alpha=0.5, calc.CI=FALSE)$p.value, #should not be significant
+    0.05)
+  expect_lte(
+    mixture.test(X0, Xm, alpha=1, calc.CI=FALSE)$p.value, #should be significant
+    0.05)
+  expect_lt(
+    mixture.test(X0, Xm)$conf.int[2],
+    1) #should exclude 1
+})
+
 .Random.seed <- old_seed
